c9a4831dd992af345aecdab05095744dcbfd1d9d
Patrick <234474225+Gliese-581c@users.noreply.github.com>
2026-01-29 16:39:53 -0800

    added documentation to shuffle algorithm

--- decklist.java
+++ decklist.java
@@ -6,11 +6,14 @@ import java.util.Collections;
 public class decklist {
     public static void main(String[] args) {
 
+        // initializes the deckbuilding materials
         ArrayList<Card> shuffler = new ArrayList<Card>();
+        Stack<Card> deck = new Stack<Card>();
         String[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
         String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
                 "King " };
 
+        // builds 52 new card objects and adds each to the shuffler
         for (int i = 0; i < suits.length; i++) {
             for (int j = 0; j < ranks.length; j++) {
                 Card card = new Card();
@@ -20,17 +23,21 @@ public class decklist {
             }
         }
 
+        // Ttis method performs the shuffle itself
         Collections.shuffle(shuffler);
 
-        Stack<Card> deck = new Stack<Card>();
+        // adds cards into deck stack
         for (Card card : shuffler) {
             deck.push(card);
         }
 
-        while (!deck.isEmpty()) {
-            Card card = deck.pop();
-            System.out.println(card.rank + " of " + card.suit);
-        }
+        /*
+         * //Print statement I used for testing
+         * while (!deck.isEmpty()) {
+         * Card card = deck.pop();
+         * System.out.println(card.rank + " of " + card.suit);
+         * }
+         */
     }
 }
 
